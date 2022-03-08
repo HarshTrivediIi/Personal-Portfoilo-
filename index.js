30f63a375f3ecd0460512162dcc6bbf042a018a0
Harsh Trivedi <651899@pdsb.net>
2022-03-08 09:39:56 -0500

    g

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-ssss
+const hamburger = document.getElementsByClassName('hamburger')[0]
+const navbarlinks = document.getElementsByClassName('nav-bar-links')[0]
+
+hamburger.addEventListener('click', () => {
+  navbarlinks.classList.toggle('active')
+})
